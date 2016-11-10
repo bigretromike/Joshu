5835bbf7fb71994e4333da4d34a2aecb6a36ffa2
bigretromike <bigretromike@monogatari.pl>
2016-11-10 14:27:02 +0100

    updated with real-data.

--- mirage/fixtures/filters.js
+++ mirage/fixtures/filters.js
@@ -1,4 +1,13 @@
 export default [
-{art: 'http://10.1.1.100:8111/JMMServerREST/GetImage/7/255', title: 'All', id: 1},
-{art: 'http://10.1.1.100:8111/JMMServerREST/GetImage/7/256', title: 'Other', id: 2}
+{id:2,name:"Missing Episodes",art:{banner:[],fanart:[{url:"http://127.0.0.1:8111/api/image/7/10718",index:0}],thumb:[{url:"http://127.0.0.1:8111/api/thumb/1/9816/0.6667",index:0}]},size:50,viewed:0,url:"http://127.0.0.1:8111/api/metadata/0/2",type:"1"},
+{id:5,name:"Votes Needed",art:{banner:[],fanart:[],thumb:[]},size:2,viewed:0,url:"http://127.0.0.1:8111/api/metadata/0/5",type:"1"},
+{id:7,name:"TvDB/MovieDB Link Missing",art:{banner:[],fanart:[],thumb:[]},size:773,viewed:0,url:"http://127.0.0.1:8111/api/metadata/0/7",type:"1"},
+{id:8,name:"Continue Watching (SYSTEM)",art:{banner:[],fanart:[],thumb:[]},size:5,viewed:0,url:"http://127.0.0.1:8111/api/metadata/0/8",type:"1"},
+{id:10,name:"ANiDB Rating",art:{banner:[],fanart:[],thumb:[]},size:2,viewed:0,url:"http://127.0.0.1:8111/api/metadata/0/10",type:"1"},
+{id:11,name:"Good Movies",art:{banner:[],fanart:[],thumb:[]},size:4,viewed:0,url:"http://127.0.0.1:8111/api/metadata/0/11",type:"1"},
+{id:12,name:"Continue Watching (SYSTEM)",art:{banner:[],fanart:[],thumb:[]},size:5,viewed:0,url:"http://127.0.0.1:8111/api/metadata/0/12",type:"2"},
+{id:13,name:"All",art:{banner:[],fanart:[],thumb:[]},size:1384,viewed:0,url:"http://127.0.0.1:8111/api/metadata/0/13",type:"4"},
+{id:14,name:"Tags",art:{banner:[],fanart:[],thumb:[]},size:0,viewed:0,url:"http://127.0.0.1:8111/api/metadata/0/14",type:"24"},
+{id:15,name:"Years",art:{banner:[],fanart:[],thumb:[]},size:0,viewed:0,url:"http://127.0.0.1:8111/api/metadata/0/15",type:"40"},
+{id:0,name:"Unsort",art:{banner:[],fanart:[{url:"http://127.0.0.1:8111/api/image/support/plex_unsort.png",index:0}],thumb:[{url:"http://127.0.0.1:8111/api/image/support/plex_unsort.png",index:0}]},size:228,viewed:0,url:"http://127.0.0.1:8111/api/metadata/1/0",type:null}]
 ];
