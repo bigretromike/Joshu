23511e8f6aa505597c3ed99c417e8a3b5eeff376
bigretromike <bigretromike@monogatari.pl>
2016-11-14 20:42:45 +0100

    color change; css tweak

--- app/models/filter.js
+++ app/models/filter.js
@@ -1,6 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	title: DS.attr(),
-	art: DS.attr()
+		name: DS.attr(),
+		art: DS.attr(),
+		url: DS.attr(),
+		size: DS.attr(),
+		viewed: DS.attr(),
+		type: DS.attr()
 });
