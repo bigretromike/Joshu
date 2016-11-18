327f148862dd7ddc0772fa00faa64e6e747f4204
bigretromike <bigretromike@monogatari.pl>
2016-11-18 11:47:17 +0100

    login and logout working correct

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    tagName: ''
+    authManager: Ember.inject.service('session'),
+	tagName: '',
+	
+	actions: {
+		invalidateSession() {
+			console.log("logout invalidateSession");
+			this.get('authManager').invalidate();
+			}
+	}
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	authManager: Ember.inject.service('session'),
 
+	actions: {
+		invalidateSession() {
+      this.get('authManager').invalidate();
+    }
+  }
 });
